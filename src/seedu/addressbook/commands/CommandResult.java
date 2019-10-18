2cfa36490f2cd3afc3e49a11f15f81f0fe38229c
L4mb0 <juan.navarro@utec.edu.pe>
2019-10-18 11:38:57 -0500

    encap

--- src/seedu/addressbook/commands/CommandResult.java
+++ src/seedu/addressbook/commands/CommandResult.java
@@ -8,7 +8,7 @@ import seedu.addressbook.data.person.ReadOnlyPerson;
 /**
  * Represents the result of a command execution.
  */
-public class CommandResult {
+private class CommandResult {
 
     /** The feedback message to be shown to the user. Contains a description of the execution result */
     public final String feedbackToUser;
