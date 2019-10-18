59b0763666707ea72593f3dcb2b7b461083c4157
giordano.alvitez@utec.edu.pe <giordano.alvitez@utec.edu.pe>
2019-10-18 12:38:35 +0100

    .

--- src/seedu/addressbook/data/person/Address.java
+++ src/seedu/addressbook/data/person/Address.java
@@ -12,11 +12,11 @@ public class Address {
     public static final String MESSAGE_ADDRESS_CONSTRAINTS = "Person addresses can be in any format";
     public static final String ADDRESS_VALIDATION_REGEX = ".+";
 
-    public final String value;
-    private Block block;
-    private Street street;
-    private Unit unit;
-    private PostalCode postalCode;
+    private final String value;
+    private Block block=new Block();
+    private Street street=new Street();
+    private Unit unit=new Unit();
+    private PostalCode postalCode=new PostalCode();
     private boolean isPrivate;
 
     /**
@@ -25,13 +25,31 @@ public class Address {
      * @throws IllegalValueException if given address string is invalid.
      */
     public Address(String address, boolean isPrivate) throws IllegalValueException {
+        block=new Block();
+        street=new Street();
+        unit=new Unit();
+        postalCode=new PostalCode();
+
         String trimmedAddress = address.trim();
+        block.setBlock("");
+        street.setStreet("");
+        unit.setUnit("");
+        postalCode.setPostalCode("");
         this.isPrivate = isPrivate;
         if (!isValidAddress(trimmedAddress)) {
             throw new IllegalValueException(MESSAGE_ADDRESS_CONSTRAINTS);
         }
-        String [] arrAddress=trimmedAddress.split(" ");
-        System.err.println("Imprimiendo direccion  "+trimmedAddress);
+        String [] arrAddress=trimmedAddress.split(",");
+        for(int i=0;i<arrAddress.length;i++){
+            if(i==0)
+                block.setBlock(arrAddress[0]);
+            else if(i==1)
+                street.setStreet(arrAddress[1]);
+            else if(i==2)
+                unit.setUnit(arrAddress[2]);
+            else if(i==3)
+                postalCode.setPostalCode(arrAddress[3]);
+        }
         this.value = trimmedAddress;
     }
 
@@ -44,7 +62,34 @@ public class Address {
 
     @Override
     public String toString() {
-        return value;
+        System.err.println("Value de conexion "+block.getBlock()+" "+street.getStreet()+" "+unit.getUnit()+" "+postalCode.getPostalCode());
+        System.err.println("Value de conexion2 "+this.value);
+        String valuetmp="";
+
+        if(block.getBlock()!="")
+            if(valuetmp!="")
+                valuetmp=valuetmp+","+block.getBlock();
+            else
+                valuetmp=block.getBlock();
+        if(street.getStreet()!="")
+            if(valuetmp!="")
+                valuetmp=valuetmp+","+street.getStreet();
+            else
+                valuetmp=street.getStreet();
+
+        if(unit.getUnit()!="")
+            if(valuetmp!="")
+                valuetmp=valuetmp+","+unit.getUnit();
+            else
+                valuetmp=unit.getUnit();
+
+        if(postalCode.getPostalCode()!="")
+            if(valuetmp!="")
+                valuetmp=valuetmp+","+postalCode.getPostalCode();
+            else
+                valuetmp=postalCode.getPostalCode();
+
+        return valuetmp;
     }
 
     @Override
