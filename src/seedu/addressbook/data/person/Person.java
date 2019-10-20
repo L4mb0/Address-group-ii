6078c2840defe96f88dd2ed27775ca91ab64f9c9
giordano.alvitez@utec.edu.pe <giordano.alvitez@utec.edu.pe>
2019-10-20 16:25:00 +0100

    Use class-level members

--- src/seedu/addressbook/data/person/Person.java
+++ src/seedu/addressbook/data/person/Person.java
@@ -16,6 +16,8 @@ public class Person implements ReadOnlyPerson {
     private Phone phone;
     private Email email;
     private Address address;
+    private int sequenceNumber;
+    private static int nextSequenceNumber=0;
 
     private final Set<Tag> tags = new HashSet<>();
 
@@ -28,6 +30,7 @@ public class Person implements ReadOnlyPerson {
         this.email = email;
         this.address = address;
         this.tags.addAll(tags);
+        this.sequenceNumber=++nextSequenceNumber;
     }
 
     /**
@@ -97,4 +100,5 @@ public class Person implements ReadOnlyPerson {
             joinPrintables+=p.toString();
         return joinPrintables;
     }
+
 }
